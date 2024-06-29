9cb80dc7aa5d00871a649232d88482898905d174
Chael15sh <mickael.bosq@orange.fr>
2024-06-29 12:48:51 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::fs::File;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    // Créer le fichier C "hello.c"
+    let mut file = File::create("hello.c").expect("Impossible de créer le fichier hello.c");
+    let c_code = r#"
+    #include <stdio.h>
+
+    int main() {
+        printf("Hello, World!\n");
+        return 0;
+    }
+    "#;
+    file.write_all(c_code.as_bytes()).expect("Impossible d'écrire dans le fichier");
+
+    println!("Le fichier hello.c a été créé avec succès !");
 }
